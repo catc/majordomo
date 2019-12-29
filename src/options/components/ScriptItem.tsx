ddcaca454eea53d22f96363dcd656df541a263c4
catalin_covic <catalin.covic@gmail.com>
2019-12-28 22:11:20 -0500

    better error handling

--- src/options/components/ScriptItem.tsx
+++ src/options/components/ScriptItem.tsx
@@ -37,9 +37,19 @@ export default function ScriptItem({
 
 	const executeScript = useCallback(() => {
 		chrome.tabs.executeScript(null, { code: script.code }, () => {
-			console.log('done executing!')
+			if (chrome.runtime.lastError) {
+				window.alert(
+					'Error executing script :: ' + chrome.runtime.lastError.message,
+				)
+				console.error(
+					`Error :: ${chrome.runtime.lastError.message}`,
+					chrome.runtime.lastError,
+				)
+			} else {
+				console.log(`Done executing "${script.name}"`)
+			}
 		})
-	}, [script.code])
+	}, [script.code, script.name])
 
 	const favourite = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.stopPropagation()
